d1af2bd1a4ae597fa8b1118b353be457310d8b7f
Kumar Kumar <kumarkumar@Kumars-MacBook-Air.local>
2024-03-11 09:19:07 +0530

    used dummy api for fetching data

--- 09social_media/src/components/PostList.jsx
+++ 09social_media/src/components/PostList.jsx
@@ -1,11 +1,24 @@
 import React, { useContext } from 'react'
 import Post from './Post'
 import { PostList as PostListData } from '../store/post-list-store'
+import WelcomeMessage from './WelcomeMessage';
 
 const PostList = () => {
-    const {postList} = useContext(PostListData);
+    const {postList, addInitialPosts} = useContext(PostListData);
+    const handlePostsClick = () => {
+        let newPostList;
+        fetch("https://dummyjson.com/posts").
+        then((response)=> response.json()).then(
+            (data)=> {
+                addInitialPosts(data.posts);
+            }
+        )
+        return newPostList;
+    }
+
   return (
     <>
+    {postList.length === 0 && <WelcomeMessage onGetPostsClick={handlePostsClick}/>}
     {postList.map((post) => <Post key={post.id} post={post}/>)}
     </>
   )
--- 09social_media/src/store/post-list-store.jsx
+++ 09social_media/src/store/post-list-store.jsx
@@ -7,6 +7,7 @@ import { createContext, useReducer } from "react";
 export const PostList = createContext({
   postList: [],
   addPost: (post) => {},
+  addInitialPosts: () => {},
   deletePost: (postId) => {},
 });
 
@@ -14,6 +15,9 @@ const postListReducer = (currPostList, action) => {
     let newPostList = currPostList;
     if (action.type === "DELETE_POST"){
         newPostList = currPostList.filter((post)=> post.id !== action.payload.postId);
+    }
+    else if (action.type === "ADD_INITIAL_POST"){
+        newPostList = action.payload.posts;
     }
     else if (action.type === "ADD_POST"){
         newPostList = [action.payload, ...currPostList];
@@ -24,19 +28,19 @@ const postListReducer = (currPostList, action) => {
 
 const PostListProvider = ({ children }) => {
   // we are going to have two actions, one for adding posts and other for deleting posts
-  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+  const addInitialPosts = (posts) => {
     dispatchPostList({
-        type: "ADD_POST",
+        type: "ADD_INITIAL_POST",
         payload: {
-            id: Date.now().toString(),
-            title: postTitle,
-            body: postBody,
-            reactions: reactions,
-            userId: userId,
-            tags: tags,
+            posts
         }
     });
   };
+
+  const addPosts = (posts)=>{
+    dispatchPostList();
+  }
+
   const deletePost = (postId) => {
     dispatchPostList({
       type: "DELETE_POST",
@@ -47,11 +51,11 @@ const PostListProvider = ({ children }) => {
   };
   const [postList, dispatchPostList] = useReducer(
     postListReducer,
-    DEFAULT_POST_LIST
+    []
   );
 
   return (
-    <PostList.Provider value={{ postList, addPost, deletePost }}>
+    <PostList.Provider value={{ postList, addPosts, addInitialPosts, deletePost }}>
       {children}
     </PostList.Provider>
   );
